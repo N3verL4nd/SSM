b9eb92e982d16c040c24cac551883cc9260a142e
N3verL4nd <lgh1992314@qq.com>
2017-07-05 13:13:37 +0800

    add json

--- src/main/java/cn/bjut/controller/TestController.java
+++ src/main/java/cn/bjut/controller/TestController.java
@@ -1,10 +1,14 @@
 package cn.bjut.controller;
 
+import cn.bjut.entity.Person;
 import cn.bjut.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * Created by N3verL4nd on 2017/7/4.
@@ -21,9 +25,16 @@ public class TestController {
 
 
     @RequestMapping("/list")
-    public String listPerson(Model model) {
+    public String listPersons(Model model) {
         model.addAttribute("persons", personService.getAllPersons());
         return "list";
+    }
+
+    //返回json
+    @ResponseBody
+    @RequestMapping("/json")
+    public List<Person> getPersons() {
+        return personService.getAllPersons();
     }
 
 }
